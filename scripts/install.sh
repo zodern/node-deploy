25f5b60c01c441f0b72fe57a4752f98ebb59575e
zodern <zodern@users.noreply.github.com>
2016-02-08 21:34:17 -0600

    Don't install phantomjs

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,15 +14,6 @@ sudo npm install -g pm2
 
 # Install PhantomJS
  sudo apt-get -y install libfreetype6 libfreetype6-dev fontconfig > /dev/null
- ARCH=`uname -m`
- PHANTOMJS_VERSION=1.9.8
-
- cd /usr/local/share/
- sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2 > /dev/null
- sudo tar xjf phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2  > /dev/null
- sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
- sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
- sudo ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/bin/phantomjs
 
 # Install python
 sudo apt-get -y install python
